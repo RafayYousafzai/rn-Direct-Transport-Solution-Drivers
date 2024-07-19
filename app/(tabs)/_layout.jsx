607b6ad47f62be8789bd6e51e4d561dceaebd769
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-07-19 09:38:35 +0500

    screens

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,76 @@
+import { Stack, Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Image, Text, View } from "react-native";
+import { icons } from "../../constants";
+
+export default function TabsLayout() {
+  const TabIcon = ({ icon, color, name, focused }) => {
+    return (
+      <View className="flex items-center justify-center gap-2">
+        <Image
+          source={icon}
+          resizeMode="contain"
+          tintColor={color}
+          className="w-6 h-6"
+        />
+        <Text
+          className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+          style={{ color: color }}
+        >
+          {name}
+        </Text>
+      </View>
+    );
+  };
+
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="Dashboard"
+          options={{
+            title: "Dashboard",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Dashboard"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="History"
+          options={{
+            title: "History",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name="History"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+}
--- app/index.jsx
+++ app/index.jsx
@@ -4,12 +4,12 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-primary">
-      <Text className="text-secondary-100 font-pextrabold text-4xl">
+      <Text className="text-secondary-200 font-pextrabold text-4xl">
         DTS Driver App.
       </Text>
       <Link
         className="bg-secondary-100 py-6 px-16 rounded-md m-4"
-        href={"/signin"}
+        href={"/Dashboard"}
       >
         Go
       </Link>
