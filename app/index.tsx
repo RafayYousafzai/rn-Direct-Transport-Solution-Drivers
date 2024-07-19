27592b4ee7a770f051c5ef0b06144582333956d8
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-07-19 09:23:52 +0500

    component

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,18 @@
-import { Button, Text, View } from "react-native";
+import { Link, useNavigation } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      <Text className="text-secondary-100 font-pextrabold text-4xl">
+        DTS Driver App.
+      </Text>
+      <Link
+        className="bg-secondary-100 py-6 px-16 rounded-md m-4"
+        href={"/signin"}
+      >
+        Go
+      </Link>
     </View>
   );
 }
