59345b7f075d71135f777bcf52679a59ab8d4748
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-08-02 11:41:05 +0500

    ui

--- app/(booking)/details.jsx
+++ app/(booking)/details.jsx
@@ -8,37 +8,62 @@ export default function Booking() {
   const { selectedBooking } = useGlobalContext();
 
   const BookingInfo = [
-    { label: "User Name", value: selectedBooking?.userName },
-    { label: "Time", value: selectedBooking?.time },
-    { label: "Service", value: selectedBooking?.service },
-    { label: "Date", value: selectedBooking?.date },
-    { label: "Contact", value: selectedBooking?.contact },
-    { label: "Job No.", value: selectedBooking?.docId },
-    { label: "User Email", value: selectedBooking?.userEmail },
+    {
+      label: "Pickup Company Name",
+      value: selectedBooking?.pickupCompanyName,
+    },
+    {
+      label: "Pickup Address",
+      value: selectedBooking?.address?.Origin?.label,
+    },
+    {
+      label: "Pickup Reference",
+      value: selectedBooking?.pickupReference1,
+    },
+    {
+      label: "Drop Company Name",
+      value: selectedBooking?.dropCompanyName,
+    },
+    {
+      label: "Delivery Address",
+      value: selectedBooking?.address?.Destination?.label,
+    },
+    {
+      label: "Delivery Instructions",
+      value: selectedBooking?.deliveryIns,
+    },
   ];
 
-  const UserInfo = [
-    { label: "User Name", value: selectedBooking?.userName },
-    { label: "Time", value: selectedBooking?.time },
-    { label: "Service", value: selectedBooking?.service },
-    { label: "Date", value: selectedBooking?.date },
-    { label: "Contact", value: selectedBooking?.contact },
+  const obInfo = [
     { label: "Job No.", value: selectedBooking?.docId },
-    { label: "User Email", value: selectedBooking?.userEmail },
+    {
+      label: "Job Type",
+      value: selectedBooking?.returnType,
+    },
+    { label: "Service", value: selectedBooking?.service },
+    { label: "Name", value: selectedBooking?.userName },
+    { label: "Email", value: selectedBooking?.userEmail },
+  ];
+  const UserInfo = [
+    { label: "Date", value: selectedBooking?.date },
+    { label: "Time", value: selectedBooking?.time },
+    { label: "Contact", value: selectedBooking?.contact },
+    { label: "Internal Reference", value: selectedBooking?.internalReference },
   ];
 
   return (
     <SafeAreaView className="px-4 bg-primary h-full">
+      <StatusBar backgroundColor="#161622" style="light" />
       <ScrollView vertical={true}>
         <View className="my-20">
-          <Text className="font-pblack text-white text-sm">
+          <Text className="font-pblack text-white text-sm mb-4">
             Booking Details
           </Text>
-          {renderList("User Details", UserInfo)}
-          {renderList("Booking Details", BookingInfo)}
+          {renderList("Job Details", obInfo)}
+          {renderList("More Details", UserInfo)}
+          {renderList("Address Details", BookingInfo)}
         </View>
       </ScrollView>
-      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
--- components/renderList.jsx
+++ components/renderList.jsx
@@ -3,18 +3,20 @@ import React from "react";
 
 export default function renderList(title, items) {
   return (
-    <>
-      <Text className="text-3xl font-pbold text-white my-4">{title}</Text>
-      {items.map((item, index) => (
-        <View className="flex flex-row my-2 " key={index}>
-          <Text className="font-psemibold text-xl text-white">
-            {item?.label || "Soon"}
-          </Text>
-          <Text className="font-psemibold ml-auto text-xl text-white">
-            {item?.value || "Soon"}
-          </Text>
-        </View>
-      ))}
-    </               >
+    <View className=" bg-gray-900 rounded-lg shadow-md">
+      <Text className="text-3xl font-bold text-white my-4">{title}</Text>
+      <View className="p-4 bg-gray-800 rounded-md">
+        {items.map((item, index) => (
+          <View className="flex flex-row items-center my-2 " key={index}>
+            <Text className="font-black text-lg text-slate-200 w-1/2 ">
+              {item?.label || "---"}{" "}
+            </Text>
+            <Text className="font-semibold ml-auto text-lg text-slate-300 w-1/2">
+              {item?.value || "---"}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </View>
   );
 }
