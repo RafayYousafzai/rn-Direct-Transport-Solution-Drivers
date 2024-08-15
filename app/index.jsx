95fa463975dc796d7a98b854361bbbe4e8840ae9
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-08-15 06:14:06 +0500

    loading optimizations

--- app/index.jsx
+++ app/index.jsx
@@ -1,53 +1,53 @@
 import React, { useEffect } from "react";
 import useGlobalContext from "@/context/GlobalProvider";
 import { useRouter } from "expo-router";
-import { Text, SafeAreaView, TouchableOpacity, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { remove } from "@/lib/SecureStore/SecureStore";
+import { ActivityIndicator, View } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
-  const { isLoggedIn, isLoading, user } = useGlobalContext();
+  const { isLoggedIn, isLoading } = useGlobalContext();
   const router = useRouter();
 
   useEffect(() => {
     const handleNavigationAndSplash = async () => {
       try {
         if (!isLoading) {
-          isLoggedIn ? router.push("Home") : router.push("signin");
+          if (isLoggedIn) {
+            router.replace("Home");
+          } else {
+            router.replace("signin");
+          }
+          await SplashScreen.hideAsync();
         }
       } catch (error) {
-        console.log(error);
-        remove("user");
-        router.navigate("signin");
-      } finally {
+        console.error("Navigation error:", error);
+        await remove("user");
+        router.replace("signin");
         await SplashScreen.hideAsync();
       }
     };
 
+    // Call navigation function when the component is mounted
     handleNavigationAndSplash();
-  }, [isLoggedIn, isLoading, user, router]);
+
+    // Add a timeout as a fallback in case of unforeseen delays
+    const timeoutId = setTimeout(() => {
+      if (!isLoading) {
+        isLoggedIn ? router.replace("Home") : router.replace("signin");
+        SplashScreen.hideAsync();
+      }
+    }, 1000); // 5 seconds fallback
+
+    // Clear timeout if component is unmounted or if navigation completes
+    return () => clearTimeout(timeoutId);
+  }, [isLoggedIn, isLoading, router]);
 
   return (
-    <SafeAreaView className="flex-1 bg-primary justify-center items-center">
-      {isLoading ? (
-        <Text className="text-slate-800 text-xl">Loading...</Text>
-      ) : (
-        <View className="w-3/4 p-4 ">
-          <Text className="text-slate-950 text-2xl font-bold mb-4 text-center">
-            Welcome!
-          </Text>
-          <TouchableOpacity
-            onPress={() => router.push(isLoggedIn ? "Home" : "signin")}
-            className="bg-secondary-100 py-3 px-6 rounded-lg mt-4"
-          >
-            <Text className="text-white text-center text-lg">
-              {isLoggedIn ? "Go to Home" : "Sign In"}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </SafeAreaView>
+    <View className="flex-1 items-center align-middle justify-center">
+      <ActivityIndicator />
+    </View>
   );
 }
