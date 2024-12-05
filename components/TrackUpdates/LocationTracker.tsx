7a881124ebf76e49d871c36db536e649831b6158
rafay <rafaykhan.mesh@gmail.com>
2024-12-05 07:50:21 +0500

    Feat: Tracking working like a charm

--- components/TrackUpdates/LocationTracker.tsx
+++ components/TrackUpdates/LocationTracker.tsx
@@ -1,14 +1,53 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import { getValueFor } from "@/lib/SecureStore/SecureStore";
+import React, { useEffect } from "react";
+import { View, Text, Button, ToastAndroid } from "react-native";
 import BackgroundGeolocation from "react-native-background-geolocation";
 
+const handleLocationUpdate = async (location: any) => {
+  ToastAndroid.show("Updating Location", ToastAndroid.SHORT);
+  try {
+    const userJson: any = await getValueFor("user");
+    const user = JSON.parse(userJson);
+    const data = JSON.stringify({ email: user.email, location });
+
+    const response = await fetch(
+      "https://direct-transport-server.vercel.app/api/locations/update_current_location",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      }
+    );
+
+    if (!response.ok) {
+      console.error("Failed to send location data:", response.statusText);
+    } else {
+      console.log("Location update successful.");
+    }
+  } catch (err: any) {
+    console.error("Error during location update:", err.message);
+  }
+};
+
 const App = () => {
+  useEffect(() => {
+    initializeGeolocation();
+
+    // Cleanup on unmount
+    return () => {
+      BackgroundGeolocation.removeAllListeners();
+      BackgroundGeolocation.stop();
+    };
+  }, []);
+
   const initializeGeolocation = () => {
     console.log("Initializing BackgroundGeolocation...");
 
     BackgroundGeolocation.ready({
       desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
-      distanceFilter: 10,
+      distanceFilter: 1,
       stopOnTerminate: false,
       startOnBoot: true,
       debug: true, // Enable sounds and verbose logs
@@ -24,18 +63,19 @@ const App = () => {
               console.error("Error starting BackgroundGeolocation:", error)
             );
         }
+
+        // Listen for location updates
+        BackgroundGeolocation.onLocation((location) => {
+          console.log("New location received:", location);
+          handleLocationUpdate(location.coords);
+        });
       })
       .catch((error) =>
         console.error("Error during BackgroundGeolocation.ready():", error)
       );
   };
 
-  return (
-    <View>
-      <Text>Background Geolocation Demo</Text>
-      <Button title="Start Geolocation" onPress={initializeGeolocation} />
-    </View>
-  );
+  return null;
 };
 
 export default App;
